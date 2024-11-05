ce03f92ee8c708198730e219b07fe332f2b79355
KIRUTHIGA <KIRUTHIGA@DESKTOP-E3UBHS1>
2024-11-05 19:22:49 +0530

    abstraction javacode

---
+++ src/com/tnsif/day8/abstraction/AbstractDemo.java
@@ -0,0 +1,26 @@
+package com.tnsif.day8.abstraction;
+
+public class AbstractDemo {
+
+	public static void main(String[] args) {
+		Square sq=new Square();
+		Rectangle rl=new Rectangle();
+		sq.calArea();
+		sq.show();
+		rl.calArea();
+		rl.show();
+		
+		System.out.println("------------------------------------------------");
+		//Runtime Polymorphism /Dynamic binding /late binding
+		
+		Shape shape;
+		shape=new Square(12.5f);
+		shape.calArea();
+		shape.show();
+		
+		shape=new Rectangle(10,20);
+		shape.calArea();
+		shape.show();
+	}
+
+}
---
+++ src/com/tnsif/day8/abstraction/Rectangle.java
@@ -0,0 +1,20 @@
+package com.tnsif.day8.abstraction;
+
+public class Rectangle extends Shape {
+		private float width, height;
+		//default constructor
+		public Rectangle() {
+			this.width=5.0f;
+			this.height=2.0f;
+		}
+		//parameterized constructor
+		public Rectangle(float width, float height) {
+			this.width=width;
+			this.height=height;
+		}
+		
+		@Override
+		void calArea() {
+			area=width*height;
+		}
+}
